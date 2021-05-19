9212af5e718481145de2464442c99e4ef72f7181
Sean Kimball <smkimbal@gmail.com>
2021-05-19 12:52:00 -0700

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,41 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <v-app>
+    <v-app-bar>
+      <v-app-bar-title>
+        <div class="d-flex align-center">
+          <v-icon style="font-size: 32px; margin-right: 12px" color="purple darken-2" large>mdi-file-document-edit-outline</v-icon>
+          NFT FUN
+        </div>
+      </v-app-bar-title>
+      <v-spacer />
+      <v-btn class="mr-1" text>Marketplace</v-btn>
+      <v-btn text>Stats</v-btn>
+      <v-divider class="mx-4" inset vertical />
+      <v-btn
+        class="mr-1"
+        icon="mdi-account-circle-outline"
+        color="transparent"
+      />
+      <v-btn
+        icon="mdi-wallet-outline"
+        color="transparent"
+      />
+    </v-app-bar>
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App'
+})
+</script>
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+html {
+  line-height: 1;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col>
+        <div style="font-size: 48px">Discover, collect, and sell extraordinary NFTs</div>
+        <div style="font-size: 24px">on the world's first & largest NFT marketplace</div>
+        <div>
+          <v-btn color="primary" flat>Explore</v-btn>
+          <v-btn color="primary" outlined>Create</v-btn>
+        </div>
+      </v-col>
+      <v-col>
+        <v-sheet>
+          <img src="https://thumbor.forbes.com/thumbor/fit-in/900x510/https://www.forbes.com/advisor/wp-content/uploads/2021/04/NFT.jpeg.jpg" />
+        </v-sheet>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
 export default defineComponent({
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
 })
 </script>
